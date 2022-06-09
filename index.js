37c58f00f602547324dc9f0079e1f5eaab111a12
Lamtev Artem <lamtev2000@gmail.com>
2022-06-09 18:52:42 +0300

    one board socket connection

---
+++ index.js
@@ -0,0 +1,24 @@
+const io = require("socket.io")(3030,{
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+let board
+let users = []
+io.on('connection', (socket) => {
+  socket.on("disconnect", () =>{
+    users = users.filter(u => u.id !== socket.id)
+    console.log("disconnect", users)
+  })
+  users.push({id : socket.id, color:"dark"})
+  if(users.length > 1){
+    users[1].color = users[0].color === "white" ? "dark" : "white"
+  }
+  socket.on("board", (arg) => {
+    board = arg;
+    socket.broadcast.emit("board", board)
+  }); 
+  if( board !== undefined ) socket.emit("firstFetch", board);
+  socket.emit("get color", users.filter(u => u.id === socket.id)[0].color);
+  console.log('a user connected',socket.id, users);
+});
